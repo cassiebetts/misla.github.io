8e43844d6b6efd47da32dc58ac8c43eca7b42364
cassiebetts <cassie@district2.co>
2017-12-16 20:58:46 -0800

    linked to paypal

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -4,26 +4,25 @@
 /*--------------------------------------------*/
 $post = (!empty($_POST)) ? true : false;
 if($post){
-    $to = "cassiebetts@gmail.com"; // Your e-mail here
+    $to = "cassiebetts@gmail.com";
 	$name = stripslashes($_POST['name']);
 	$email = stripslashes($_POST['email']);
 	$subject = stripslashes($_POST['subject']);
 	$message = stripslashes($_POST['message']);
 	$subject = "From Your Website";
-	
+
 	$mail = mail($to, $subject,
-         "Name: ".$name."\r\n"	
-         ."Email: ".$email."\r\n"	
-         ."subject: ".$subject."\r\n"	
-         ."Message: ".$message."\r\n"	
+         "Name: ".$name."\r\n"
+         ."Email: ".$email."\r\n"
+         ."subject: ".$subject."\r\n"
+         ."Message: ".$message."\r\n"
 	    ."X-Mailer: PHP/" . phpversion());
 	if($mail){
-		echo '<p>Thanks for submitting your email! Our manager will contact you shortly.</p>';
+		echo '<p>Thanks for submitting your email! Someone will contact you shortly.</p>';
 	}
-    
+
 }
 ?>
-
     <style type="text/css">
         p {
             text-align: center;
