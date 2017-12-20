11a50647f4ec86773ada45838731490c10f2c177
Christopher Clarke <afroradiohead@gmail.com>
2017-12-19 17:28:24 -0800

    add send email

--- server.js
+++ server.js
@@ -1,34 +1,52 @@
 // server.js
 require('dotenv').config();
 const compression = require('compression');
-const fs = require('fs');
-const cheerio = require('cheerio');
+const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
+const _ = require('lodash');
 const app = express();
 const httpServer = app.listen(process.env.PORT || 8080);
 const _build_dir = `${__dirname}/build`;
-const Mailgun = require('mailgun').Mailgun;
+const nodemailer = require('nodemailer');
+const moment = require('moment');
 
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.set('httpServer', httpServer);
 app.use(compression());
 
 app.use(express.static(path.resolve(_build_dir)));
 
 app
-    .post('/contact', function(req, res) {
-        const mailgun = new Mailgun('key-591b36ece58b76011a55318d2187379d');
-        mg.sendText('web-bx9t5@mail-tester.com', ['web-bx9t5@mail-tester.com'],
-            'This is the subject',
-            'This is the text',
-            'noreply@example.com', {},
-            function(err) {
-                if (err) console.log('Oh noes: ' + err);
-                else     console.log('Success');
+    .post('/api/contact', function(req, res) {
+        const email = 'contact.misla@gmail.com';
+        const fromEmail = _.get(req.body, 'email');
+        const subject = _.get(req.body, 'subject', "No Subject");
+        const message = _.get(req.body, 'message', "No Message");
+        const timestamp = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+
+        if(email){
+            const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: email,
+                    pass: 'Pn9RXoky31nZ'
+                }
             });
-        res.send('<p>Thanks for submitting your email! Someone will contact you shortly.</p>');
+            transporter.sendMail({
+                from: fromEmail,
+                to: email,
+                subject: "New Lead from Misla.org",
+                html: `
+                    <p><b>Timestamp: </b> ${timestamp}</p>
+                    <p><b>From: </b> ${fromEmail}</p>
+                    <p><b>Subject: </b> ${subject}</p>
+                    <p><b>Message: </b> ${message}</p>
+                `
+            }, function(error, info){});
+        }
+
+        res.send('<html><head><title>Thank You</title></tite></head><body><p>Thanks for submitting your email! Someone will contact you shortly.</p></body></html>');
         res.end();
-    })
-    .get('*', (req, res) => {
-    res.sendFile(path.resolve(_build_dir, 'index.html'));
-});
+    });
